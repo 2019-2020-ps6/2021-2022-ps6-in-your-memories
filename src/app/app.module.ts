858429fa19dfbc35c4cb251b8da589d56247e05d
emmadeal <emma.deal@etu.unice.fr>
2022-03-22 10:51:24 +0100

    creation files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { QuizEditComponent } from './quizzes/quiz-edit/quiz-edit.component';
+import { QuizFormComponent } from './components/quizzes/quiz-form/quiz-form.component';
+import { QuizListComponent } from './components/quizzes/quiz-list/quiz-list.component';
+import { QuizComponent } from './components/quizzes/quiz/quiz.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuizEditComponent,
+    QuizFormComponent,
+    QuizListComponent,
+    QuizComponent
   ],
   imports: [
     BrowserModule
