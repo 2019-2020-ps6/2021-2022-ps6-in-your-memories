86e623192303bddae96d4fda423c155ab0b69b61
AurélienARNAULT <70631925+AurelienARNAULT@users.noreply.github.com>
2022-03-22 11:36:28 +0100

    Ajout route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { QuizEditComponent } from './quizzes/quiz-edit/quiz-edit.component';
+import { QuizEditComponent } from './components/quizzes/quiz-edit/quiz-edit.component';
 import { QuizFormComponent } from './components/quizzes/quiz-form/quiz-form.component';
 import { QuizListComponent } from './components/quizzes/quiz-list/quiz-list.component';
+import { AppRoutingModule } from './app.routing.module';
 import { QuizComponent } from './components/quizzes/quiz/quiz.component';
 
 @NgModule({
@@ -16,6 +17,7 @@ import { QuizComponent } from './components/quizzes/quiz/quiz.component';
     QuizComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule
   ],
   providers: [],
---
+++ src/app/app.routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { QuizListComponent } from './components/quizzes/quiz-list/quiz-list.component';
+import { QuizEditComponent } from './components/quizzes/quiz-edit/quiz-edit.component';
+
+const routes: Routes = [
+    {path: 'quiz-list', component: QuizListComponent},
+    {path: 'edit-quiz/:id', component: QuizEditComponent},
+    { path: '', redirectTo: '/quiz-list', pathMatch: 'full' },
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {
+
+}
