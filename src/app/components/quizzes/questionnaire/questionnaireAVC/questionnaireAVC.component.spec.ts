beb29658011ca583169b72af4fc6de961cd7e9cb
AurélienARNAULT <70631925+AurelienARNAULT@users.noreply.github.com>
2022-04-04 20:29:28 +0200

    Update questionnaireAVC.component.spec.ts

--- src/app/components/quizzes/questionnaire/questionnaireAVC/questionnaireAVC.component.spec.ts
+++ src/app/components/quizzes/questionnaire/questionnaireAVC/questionnaireAVC.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { QuestionnaireAVCComponent } from './questionnaireAVC.component.ts';
+import { QuestionnaireAVCComponent } from './questionnaireAVC.component';
 
 describe('QuestionnaireComponent', () => {
   let component: QuestionnaireAVCComponent;
