df5489bcf70424fae5f817818e19a442fa2af1f2
arnault aurelien <aurelien.arnault@etu.univ-cotedazur.fr>
2022-04-24 19:44:32 +0200

    correction erreur quiz

--- back-end/app/models/quiz.model.js
+++ back-end/app/models/quiz.model.js
@@ -2,6 +2,8 @@ const Joi = require('joi')
 const BaseModel = require('../utils/base-model.js')
 
 module.exports = new BaseModel('Quiz', {
+  id: Joi.string(),
   theme: Joi.string().required(),
   name: Joi.string().required(),
+  questions: Joi.any(),
 })
